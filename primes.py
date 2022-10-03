81f1a2125a6b79e249b9c6e0792f5821a6a20aeb
Bilkhoo, Aman (k21013503) <aman.bilkhoo@kcl.ac.uk>
2022-10-03 10:32:03 +0100

    Complete exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    #Ignore possibility number_of_primes < 1
     list = []
+    
+    i = 2
+    while len(list) < number_of_primes:
+        prime = True;
+        for item in list:
+            if i % item == 0:
+                prime = False;
+                break;
+        
+        if prime == True:
+            list.append(i)
+        i += 1
+
     return list
